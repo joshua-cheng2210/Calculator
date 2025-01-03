f7170cc02d8e91208d22868310d1ef3f51fcc45d
chen7647 <chen7647@umn.edu>
2025-01-03 17:42:10 -0600

    changed based

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,18 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    base: './', // Ensure the base path is set correctly for GitHub Pages
+    base: './', // Replace 'Calculator' with your repository name
     outDir: 'dist',
     rollupOptions: {
       input: {
         main: './index.html'
       },
       output: {
-        entryFileNames: 'assets/index.js', // Set a predictable name for the entry file
-        chunkFileNames: 'assets/[name].js', // Set a predictable name for chunk files
-        assetFileNames: 'assets/[name].[ext]' // Set a predictable name for asset files
+        entryFileNames: 'assets/index.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     },
-    chunkSizeWarningLimit: 1000 // Adjust the chunk size warning limit if needed
+    chunkSizeWarningLimit: 1000
   }
 });
