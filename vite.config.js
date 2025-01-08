f94dc3de43793601cb255e6bebc926a7429f2aeb
chen7647 <chen7647@umn.edu>
2025-01-08 15:48:05 -0600

    launching

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    base: './', // Replace 'Calculator' with your repository name
+    base: '/Calculator/', // Replace 'Calculator' with your repository name
     outDir: 'dist',
     rollupOptions: {
       input: {
