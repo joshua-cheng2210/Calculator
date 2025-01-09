be4f06a3d8b0d68caae6e2920385674cdf9858f7
chen7647 <chen7647@umn.edu>
2025-01-08 19:58:27 -0600

    launching

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Calculator/',
   plugins: [react()],
   build: {
-    base: '/Calculator/', // Replace 'Calculator' with your repository name
     outDir: 'dist',
     rollupOptions: {
       input: {
         main: './index.html'
       },
       output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
+        entryFileNames: 'assets/index.js', // Set a predictable name for the entry file
+        chunkFileNames: 'assets/[name].js', // Set a predictable name for chunk files
+        assetFileNames: 'assets/[name].[ext]' // Set a predictable name for asset files
       }
     },
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 1000 // Adjust the chunk size warning limit if needed
+    
   }
 });
